5f3ce550d6bada3c3819d6228628d845ddf884a6
Ouali Cherikh <ouali.cherikh@gmail.com>
2020-02-05 11:33:07 +0100

    update

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ myRouter.route('/reservation/:reservation_id')
     var reservationUser = Reservation.find({"userUid": req.params.reservation_id }, {"_id": 0, userUid: 0,statut: 0, __v: 0});
     console.log(reservationUser);
     var peopleArray = Object.values(reservationUser)
-    Event.find({_id: {$in: peopleArray } }, function(err, events){
+    Event.find({_id: {$in:  mongoose.Types.ObjectId(peopleArray)} }, function(err, events){
         if (err){
             res.send(err); 
         }
