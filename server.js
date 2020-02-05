14d28b1d5764fe8cfc445d7e4f16a74012d7d787
Ouali Cherikh <ouali.cherikh@gmail.com>
2020-02-05 11:23:50 +0100

    update

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ myRouter.route('/reservation/:reservation_id')
 .get(function(req,res){ 
     var reservationUser = Reservation.find({"userUid": req.params.reservation_id }, {"_id": 0, userUid: 0,statut: 0, __v: 0});
     console.log(reservationUser);
-    Event.find({_id: {$in: reservationUser }}, function(err, events){
+    Event.find({"_id": {$in: reservationUser }}, function(err, events){
         if (err){
             res.send(err); 
         }
