d872e32fa5a01bbf6feb1b84402b71f3d458c7f8
Ouali Cherikh <ouali.cherikh@gmail.com>
2020-02-05 11:54:42 +0100

    update

--- server.js
+++ server.js
@@ -160,8 +160,8 @@ myRouter.route('/reservations')
 myRouter.route('/reservation/:reservation_id')
 .get(function(req,res){ 
     var reservationUser = Reservation.find({"userUid": req.params.reservation_id }, {"_id": 0, userUid: 0,statut: 0, __v: 0});
-    let objetIdArray = reservationUser.map(s => mongoose.Types.ObjectId(s));
-    Event.find({"_id": {$in:  objetIdArray }}, function(err, events){
+    const peopleArray = reservationUser.keys(peopleObj).map(i => peopleObj[i])
+    Event.find({_id: {$in:  peopleArray }}, function(err, events){
         if (err){
             res.send(err); 
         }
