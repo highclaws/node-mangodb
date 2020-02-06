728a71579e1cfb3ebfc440000499c048b7d14f8a
Ouali Cherikh <ouali.cherikh@gmail.com>
2020-02-06 22:17:49 +0100

    update

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ myRouter.route('/reservations')
       }); 
 }); 
 
-myRouter.route('/reservation/:reservation_id')
+myRouter.route('/reservations/:reservation_id')
 .get(function(req,res){ 
     //var reservationUser = Reservation.find({"userUid": req.params.reservation_id }, {"_id": 0, userUid: 0,statut: 0, __v: 0});
     //console.log(reservationUser);
@@ -183,7 +183,6 @@ myRouter.route('/reservation/:reservation_id')
             }); 
         }
     });
-
 })
 .put(function(req,res){ 
     Reservation.findById(req.params.reservation_id, function(err, reservation) {
